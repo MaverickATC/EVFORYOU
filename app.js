6edc38fe3ec300d3f9132993a565bb9e02634fd2
MaverickATC <atc1991@rocketmail.com>
2020-05-30 02:01:36 +0300

    predeploy7

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-//const path = require('path');
+//for heroku
+const path = require('path');
+//
 const config = require("config");
 const mongoose = require("mongoose");
 
